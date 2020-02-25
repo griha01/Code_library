a3fdf865ba34f43818cfebcfb04fc6c52cec0519
griha01 <49586680+griha01@users.noreply.github.com>
2020-02-25 11:09:51 +0000

    Update CircleArray.cpp

--- C++/CircleArray.cpp
+++ C++/CircleArray.cpp
@@ -4,12 +4,7 @@
 
 using namespace std;
 
-bool get(double* A, int size,int &tail,int head,double &a)
-{
-	if (head == 0 && tail == 0) return false;
-
-}
-bool set(double a,double* A,int tail, int &head, int N)
+bool set(double* A, int N, int tail, int& head, double a)
 {
 	if (head != tail)
 	{
@@ -18,6 +13,15 @@ bool set(double a,double* A,int tail, int &head, int N)
 		return true;
 	}
 	return false;
+}
+bool get(double* A, int N, int& tail, int head, double& a)
+{
+	if (head == 0 && tail == 0) return false;
+	cout << "tail:" << tail << endl;
+	a = A[tail];
+	if (tail + 1 == N) tail = 0;
+	else tail++;
+	return true;
 }
 void show(double* A,int tail,int head,int N) {
 	while (tail != head )
@@ -31,12 +35,18 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	const int N = 6;
+	double number = 0;
 	double A[N] = {88,99, -9, -10, 33, 77};
-	int tail = 4, head = 2;
+	int tail = 5, head = 2;
 	show(A, tail, head, N);
-	cout << endl <<set(10, A, tail, head,N) << endl;
+	cout << endl <<set(A,N, tail, head,10) << endl;
 	cout << endl;
 	show(A, tail, head, N);
+	cout << endl << get(A, N, tail, head, number) << endl;
+	cout << endl;
+	show(A, tail, head, N);
+
+
 
 	system("pause");
 }
