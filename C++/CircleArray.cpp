4c996eb3c6160220ced1c39bd0dee4a5bc91174a
griha01 <49586680+griha01@users.noreply.github.com>
2020-02-25 13:07:29 +0000

    Update CircleArray.cpp

--- C++/CircleArray.cpp
+++ C++/CircleArray.cpp
@@ -4,31 +4,50 @@
 
 using namespace std;
 
-bool set(double* A, int N, int tail, int& head, double a)
+void set(double* A, int N, int& flag, int tail, int& head, double a)
 {
-	if (head != tail)
-	{
-		A[head++] = a;
-		if (head == N) head = 0;
-		return true;
-	}
-	return false;
+	if (flag == -2) return;
+	A[head++] = a;
+	if (head == N) head = 0;
+	flag=(head == tail)?-2:0;
 }
-bool get(double* A, int N, int& tail, int head, double& a)
+
+void get(double* A, int N, int& flag, int& tail, int head, double& a)
 {
-	if (head == 0 && tail == 0) return false;
-	cout << "tail:" << tail << endl;
-	a = A[tail];
-	if (tail + 1 == N) tail = 0;
-	else tail++;
-	return true;
-}
-void show(double* A,int tail,int head,int N) {
-	while (tail != head )
-	{
-		cout << A[tail++] << " " << endl;
-		if (tail == N) tail = 0;
+	if (flag == -1)return;
+	if (flag == -2) {
+		a = A[tail++];
+		if (tail == N)tail = 0;
+		flag = 0;
 	}
+	if (flag == 0) {
+		a = A[tail++];
+		if (tail == N)tail = 0;
+		if (tail == head) flag = -1;
+	}
+/*
+	if (flag == -1)return;
+	a = A[tail++];
+	if (tail == N)tail = 0;
+	flag = (tail == head) ? -1:0
+	*/
+
+}
+void show(double* A, int N, int flag, int tail,int head) {
+	if (flag == -1) return;
+	if (flag==0)
+		while (tail != head )
+		{
+			cout << A[tail++] << "\t";
+			if (tail == N) tail = 0;
+		}
+	else
+		do 
+		{
+			cout << A[tail++] << "\t";
+			if (tail == N) tail = 0;
+		} while (tail != head);
+		cout << endl;
 }
 int main()
 {
@@ -36,17 +55,29 @@ int main()
 	SetConsoleOutputCP(1251);
 	const int N = 6;
 	double number = 0;
+	int flag = 0;
 	double A[N] = {88,99, -9, -10, 33, 77};
-	int tail = 5, head = 2;
-	show(A, tail, head, N);
-	cout << endl <<set(A,N, tail, head,10) << endl;
+	int tail = 4, head = 2;
+	show(A, N, flag, tail,head);
+
+	double B[N] = { 88,99, -9, -10, 33, 77 };
+	show(B, N, -2, 3, 3);
+
+	double C[N] = { 88,99, -9, -10, 33, 77 };
+	flag = -1;
+	head = tail = 5;
+	set(C, N, flag, tail, head, 10);
+	show(C, N, flag, tail, head);
+
+	get(C, N, flag, tail, head, number);
+	show(C, N, flag, tail, head);
+	cout << "Number:" << number << endl;
+	/*cout << endl <<set(A,N, tail, head,10) << endl;
 	cout << endl;
 	show(A, tail, head, N);
 	cout << endl << get(A, N, tail, head, number) << endl;
 	cout << endl;
 	show(A, tail, head, N);
-
-
-
+	cout << endl;*/
 	system("pause");
 }
