2565e56afee5ff1e9979e09ed460554998620fad
griha01 <49586680+griha01@users.noreply.github.com>
2020-02-25 10:30:09 +0000

    Update CircleArray.cpp

--- C++/CircleArray.cpp
+++ C++/CircleArray.cpp
@@ -13,19 +13,20 @@ bool set(double* A, int size, int& tail, int head, double a)
 {
 	return true;
 }
-void show(double* A,int tail,int head) {
-	for (int i = tail; i < head; i++)
+void show(double* A,int tail,int head,int N) {
+	while (tail != head )
 	{
-		cout << A[i] << " " << endl;
+		cout << A[tail++] << " " << endl;
+		if (tail == N) tail = 0;
 	}
 }
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	const int N = 10;
-	double A[N] = {33,77,88};
-	int tail = 0, head = 3;
-	show(A, tail, head);
+	const int N = 6;
+	double A[N] = {88,99, -9, -10, 33, 77};
+	int tail = 4, head = 2;
+	show(A, tail, head, N);
 	system("pause");
 }
